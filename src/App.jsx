e84d2c4f95df23b5502d4369a43ee735d0a88aaf
shenaz130 <fathimashenaz25@gmail.com>
2025-02-25 20:04:30 -0500

    v2 updated

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,22 @@ function App() {
       const NASA_KEY =import.meta.env.VITE_NASA_API_KEY
       const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
 
+      const today = (new Date()).toDateString()
+      const localKey =`NASA-${today}`
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey))
+        setData(apiData)
+        console.log('fetched from API today')
+        return
+      }
+      localStorage.clear()
+
       try {
         const res = await fetch(url);
         const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData))
         setData(apiData)
-        console.log('DATA\n', apiData);
+        console.log('fetched from API today')
       } catch (err) {
         console.log(err.message);
       }
